f7fd9cf099e3841f2efeac1d091bfe657fa64fe4
Jacqui Ahn <jsahn23@gmail.com>
2016-09-08 15:28:43 -0400

    adds routes, updates Home.js and index.js

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+// Developer: Define App component defintion here
+import React, {Component} from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/config/routes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {Route} from 'react-router'
+import Home from '../Home'
+
+module.exports = (
+  <Route path='/' component={Home}/>
+)
